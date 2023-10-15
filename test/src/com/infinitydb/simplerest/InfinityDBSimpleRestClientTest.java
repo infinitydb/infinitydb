1e1f00f5055c793db54b661a0b9d8a15194e8ddf
Boiler Bay <rlderan2@boilerbay.com>
2023-10-14 18:58:31 -0700

    Update InfinityDBSimpleRestClientTest.java
    Use junit

--- test/src/com/infinitydb/simplerest/InfinityDBSimpleRestClientTest.java
+++ test/src/com/infinitydb/simplerest/InfinityDBSimpleRestClientTest.java
@@ -33,23 +33,212 @@ import org.junit.Test;
  * and boilerbay.com.
  */
 public class InfinityDBSimpleRestClientTest {
-    static final String INFINITYDB_HOST =
-          "https://infinitydb.com:37411/infinitydb/data/demo/writeable";
-    static final String LOCAL_HOST =
-            "http://localhost:37411/infinitydb/data/demo/writeable";
-    static final String HOST = INFINITYDB_HOST;
-
+    
+    static class Target {
+        String host;
+        String userName;
+        String passWord;
+        boolean isDisableSSLSecurity;
+        Target(String host, String userName, String passWord, boolean isDisableSSLSecurity) {
+            this.host = host;
+            this.userName = userName;
+            this.passWord = passWord;
+            this.isDisableSSLSecurity = isDisableSSLSecurity;
+        }
+    }
+    
+    static final Target INFINITYDB = new Target(
+            "https://infinitydb.com:37411/infinitydb/data/demo/writeable", 
+            "testUser", "db",
+            false);
+    static final Target LOCAL = new Target(
+            "http://localhost:37411/infinitydb/data/demo/writeable",
+            "testUser", "db",
+             false);
+    static final Target TARGET = INFINITYDB;
+    
     static InfinityDBSimpleRestClient idb;
     
     static InfinityDBSimpleRestClient getClient( ) {
         if (idb != null)
             return idb;
-        System.out.println("host=" + HOST);
-        idb = new InfinityDBSimpleRestClient(HOST);
-        idb.setUserNameAndPassWord("testUser", "db");
+        
+        System.out.println("host=" + TARGET.host);
+        idb = new InfinityDBSimpleRestClient(TARGET.host);
+        
+        idb.setUserNameAndPassWord(TARGET.userName, TARGET.passWord);
+        /*
+         * Do this if you have an https server but the host name is not
+         * verifiable due to lack of an installed X509 certificate there. See
+         * the InfinityDB Server documentation at boilerbay.com. Installing a
+         * certificate requires root access - look in
+         * /data/infinitydb-home/key-store. Use certificate-tool.py to help you
+         * either use letsEncrypt, self-signed, or external.
+         * 
+         * This is rather slow, because it breaks keep-alive. That should be
+         * encouragement for getting the certificate in the server.
+         */
+        if (TARGET.isDisableSSLSecurity)
+            idb.setDisableSSLSecurity(true);
         return idb;
     }
     
+    // Dozens of queries return. They have many different component types
+    @Test
+    public void testGetAllQueries() throws Exception {
+        testGet("get all queries", new IdbClass("Query"));
+    }
+
+    // A single string returns
+    @Test
+    public void testGetAStringFromTheDocumentation() throws Exception {
+        testGet("get one line",
+                new IdbClass("Documentation"),"Basics",
+                new IdbAttribute("description"), new IdbIndex(0));
+    }
+
+    public void testGet(String msg, Object... item) throws Exception {
+        InfinityDBSimpleRestClient idb = getClient();
+        PrintTime t = new PrintTime();
+        Blob response = null;
+        for (int i = 0; i < 3; i++) {
+            response = idb.get(item);
+            t.printTime(msg, response.length());
+        }
+        String contentType = response.getContentType();
+        Assert.assertEquals("application/json", contentType);
+        String s = response.toString();
+        // Throws if bad
+        JsonElement root = new JsonParser(s).parse();
+    }
+
+    @Test
+    public void testPutBlob() throws Exception {
+        InfinityDBSimpleRestClient idb = getClient();
+        PrintTime t = new PrintTime();
+
+        for (int i = 0; i < 3; i++) {
+            Blob request = new Blob("\"hello from Java\"", "application/json");
+            idb.putBlob(request, new IdbClass("Trash"), new IdbClass("JavaDemo"),
+                    new IdbClass("DemoPost"), new Date());
+            t.printTime("put blob", request.length());
+        }
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        testExecute("exec get image",
+                new JsonObject(false, new IdbAttribute("name"), "pic0"),
+                "examples", "Display Image", "application/json");
+    }
+    
+    public void testExecute(String msg, JsonObject jsonObject, String ifc, String method,
+            String expectedContentType) throws Exception {
+        InfinityDBSimpleRestClient idb = getClient();
+
+        PrintTime t = new PrintTime();
+        Blob response = null;
+        for (int i = 0; i < 3; i++) {
+            Blob requestBlob = new Blob(jsonObject);
+             response = idb.executeQuery(
+                ifc, method, requestBlob, null);
+            t.printTime(msg, response.length());
+        }
+        String contentType = response.getContentType();
+        Assert.assertEquals(expectedContentType, contentType);
+        checkJsonParsing(response);
+    }
+        
+    @Test
+    public void testExecuteGetBlobQueryImage() throws Exception {
+        testExecuteGetBlobQuery("exec get blob image ",
+                new Blob("{ \"_name\" : \"pic0\" }", "application/json"),
+                "examples", "Display Image", "image/jpeg");
+    }
+    
+    public void testExecuteGetBlobQuery(String msg, Blob requestBlob, String ifc, String method,
+            String expectedContentType) throws Exception {
+        InfinityDBSimpleRestClient idb = getClient();
+        PrintTime t = new PrintTime();
+
+        Blob response = null;
+        for (int i = 0; i < 3; i++) {
+             response = idb.executeGetBlobQuery(
+                ifc, method, requestBlob, null);
+            t.printTime(msg, response.length());
+        }
+        String contentType = response.getContentType();
+        Assert.assertEquals(expectedContentType, contentType);
+    }
+
+    @Test
+    public void testExecutePutBlobQuery() throws Exception {
+        testExecutePutBlobQuery("exec put blob ",
+                "examples", "Put blob by name",
+                new Blob("I am a plain text blob"),
+                new Blob("{ \"_name\" : \"my blob\" }", "application/json"),
+                "application/json");
+    }
+
+    public void testExecutePutBlobQuery(String msg, 
+            String ifc, String method,
+            Blob requestBlob, 
+            Blob paramsBlob,
+            String expectedContentType) throws Exception {
+        InfinityDBSimpleRestClient idb = getClient();
+        PrintTime t = new PrintTime();
+
+        Blob response = null;
+        for (int i = 0; i < 3; i++) {
+             response = idb.executePutBlobQuery(
+                ifc, method, requestBlob, paramsBlob);
+            t.printTime(msg, requestBlob.length());
+        }
+        String contentType = response.getContentType();
+//        System.out.println("content type " + contentType);
+        Assert.assertEquals(expectedContentType, contentType);
+    }
+
+    void checkJsonParsing(Blob response) {
+        PrintTime t = new PrintTime();
+        String s = response.toString();
+        t.printTime("response.toString()", response.length());
+        // System.out.println("Response: "+ s);
+
+        JsonElement root = new JsonParser(s).parse();
+        t.printTime("parse", response.length());
+
+        String formatted = root.toString();
+        //String formatted = root.toStringAsExtendedFormatIndented();
+//        System.out.println("Formatted: " + formatted);
+        t.printTime("root.toString()", formatted.length());
+
+        // Do another round to see if it stays the same 
+        JsonElement root2 = new JsonParser(formatted).parse();
+//        String formatted2 = root2.toString();
+//        System.out.println("Formatted2: " + formatted2);
+        Assert.assertEquals(root, root2); 
+
+        /*
+         * You can print in the special 'extended JSON' format specific to
+         * InfinityDB in which we avoid quoting keys as double-quoted strings. 
+         * There is a display mode in the browser for it, and it is
+         * quite relaxing. 
+         */
+        String formattedExtended = root.toStringAsExtendedFormat();
+        t.printTime("toStringAsExtended", formattedExtended.length());
+//            System.out.println("Extended Format: " + formattedExtended);
+        JsonElement rootExtended = new JsonParser(formattedExtended, false).parse();
+        t.printTime("parse ExtendedFormat", formattedExtended.length());
+        
+        // Do another round to see if it is consistent.
+        String formattedExtended2 = rootExtended.toStringAsExtendedFormat();
+//            System.out.println("Extended Format re-formatted: " + formattedExtended2);
+        t.printTime("toStringAsExtended", formattedExtended2.length());
+
+         Assert.assertEquals(root, rootExtended);
+    }
+
     @Test
     public void testJson() {
         JsonElement e0 = new JsonObject(false, new IdbAttribute("att"), new Long(5), "hi");
@@ -110,165 +299,10 @@ public class InfinityDBSimpleRestClientTest {
         e0.insert(true, new IdbClass("Ec"), new Long(5), new Double(5));
         List<List<Object>> items = e0.flattenToList();
         for (int i = 0; i < items.size(); i++)
-            System.out.println("items[i] " + items.get(i));
+//            System.out.println("items[i] " + items.get(i));
         Assert.assertEquals(items.size(), 2);
         JsonElement e1 = JsonElement.unflattenFromList(true, items);
         Assert.assertEquals(e0, e1);
-    }
-
-    // Dozens of queries return. They have many different component types
-    @Test
-    public void testGetAllQueries() throws Exception {
-        testGet("All queries direct", new IdbClass("Query"));
-    }
-
-    // A single string returns
-    @Test
-    public void testGetAStringFromTheDocumentation() throws Exception {
-        testGet("Small direct",
-                new IdbClass("Documentation"),"Basics",
-                new IdbAttribute("description"), new IdbIndex(0));
-    }
-
-    public void testGet(String msg, Object... item) throws Exception {
-        PrintTime t = new PrintTime();
-        InfinityDBSimpleRestClient idb = getClient();
-        Blob response = null;
-        for (int i = 0; i < 3; i++) {
-            response = idb.get(item);
-            t.printTime(msg, response.length());
-        }
-        String contentType = response.getContentType();
-        Assert.assertEquals("application/json", contentType);
-        String s = response.toString();
-        // Throws if bad
-        JsonElement root = new JsonParser(s).parse();
-    }
-
-    @Test
-    public void testPutBlob() throws Exception {
-        PrintTime t = new PrintTime();
-        InfinityDBSimpleRestClient idb = getClient();
-
-        Blob request = new Blob("\"hello from Java\"", "application/json");
-        idb.putBlob(request, new IdbClass("Trash"), new IdbClass("JavaDemo"),
-                new IdbClass("DemoPost"), new Date());
-        
-        t.printTime("putBlob", 0);
-    }
-
-    @Test
-    public void testExecute() throws Exception {
-        testExecute("exec get image(not getblob)",
-                new JsonObject(false, new IdbAttribute("name"), "pic0"),
-                "examples", "Display Image", "application/json");
-    }
-    
-    public void testExecute(String msg, JsonObject jsonObject, String ifc, String method,
-            String expectedContentType) throws Exception {
-        InfinityDBSimpleRestClient idb = getClient();
-
-        PrintTime t = new PrintTime();
-        Blob response = null;
-        for (int i = 0; i < 3; i++) {
-            Blob requestBlob = new Blob(jsonObject);
-             response = idb.executeQuery(
-                ifc, method, requestBlob, null);
-            t.printTime(msg, response.length());
-        }
-        String contentType = response.getContentType();
-        Assert.assertEquals(expectedContentType, contentType);
-        checkJsonParsing(response);
-    }
-        
-    @Test
-    public void testExecuteGetBlobQueryImage() throws Exception {
-        testExecuteGetBlobQuery("exec get blob image ",
-                new Blob("{ \"_name\" : \"pic0\" }", "application/json"),
-                "examples", "Display Image", "image/jpeg");
-    }
-    
-    public void testExecuteGetBlobQuery(String msg, Blob requestBlob, String ifc, String method,
-            String expectedContentType) throws Exception {
-        InfinityDBSimpleRestClient idb = getClient();
-        PrintTime t = new PrintTime();
-
-        Blob response = null;
-        for (int i = 0; i < 3; i++) {
-             response = idb.executeGetBlobQuery(
-                ifc, method, requestBlob, null);
-            t.printTime(msg, response.length());
-        }
-        String contentType = response.getContentType();
-        Assert.assertEquals(expectedContentType, contentType);
-    }
-
-    @Test
-    public void testExecutePutBlobQuery() throws Exception {
-        testExecutePutBlobQuery("exec put blob query ",
-                "examples", "Put blob by name",
-                new Blob("I am a plain text blob"),
-                new Blob("{ \"_name\" : \"my blob\" }", "application/json"),
-                "application/json");
-    }
-
-    public void testExecutePutBlobQuery(String msg, 
-            String ifc, String method,
-            Blob requestBlob, 
-            Blob paramsBlob,
-            String expectedContentType) throws Exception {
-        InfinityDBSimpleRestClient idb = getClient();
-        PrintTime t = new PrintTime();
-
-        Blob response = null;
-        for (int i = 0; i < 3; i++) {
-             response = idb.executePutBlobQuery(
-                ifc, method, requestBlob, paramsBlob);
-            t.printTime(msg, response.length());
-        }
-        String contentType = response.getContentType();
-        System.out.println("content type " + contentType);
-        Assert.assertEquals(expectedContentType, contentType);
-    }
-
-    void checkJsonParsing(Blob response) {
-        PrintTime t = new PrintTime();
-        String s = response.toString();
-        t.printTime("response.toString()", response.length());
-        // System.out.println("Response: "+ s);
-
-        JsonElement root = new JsonParser(s).parse();
-        t.printTime("parse", response.length());
-
-        String formatted = root.toString();
-        //String formatted = root.toStringAsExtendedFormatIndented();
-//        System.out.println("Formatted: " + formatted);
-        t.printTime("root.toString()", formatted.length());
-
-        // Do another round to see if it stays the same 
-        JsonElement root2 = new JsonParser(formatted).parse();
-//        String formatted2 = root2.toString();
-//        System.out.println("Formatted2: " + formatted2);
-        Assert.assertEquals(root, root2); 
-
-        /*
-         * You can print in the special 'extended JSON' format specific to
-         * InfinityDB in which we avoid quoting keys as double-quoted strings. 
-         * There is a display mode in the browser for it, and it is
-         * quite relaxing. 
-         */
-        String formattedExtended = root.toStringAsExtendedFormat();
-        t.printTime("toStringAsExtendedFormat()", formattedExtended.length());
-//            System.out.println("Extended Format: " + formattedExtended);
-        JsonElement rootExtended = new JsonParser(formattedExtended, false).parse();
-        t.printTime("parse ExtendedFormat", formattedExtended.length());
-        
-        // Do another round to see if it is consistent.
-        String formattedExtended2 = rootExtended.toStringAsExtendedFormat();
-//            System.out.println("Extended Format re-formatted: " + formattedExtended2);
-        t.printTime("toStringAsExtendedFormat() again", formattedExtended2.length());
-
-         Assert.assertEquals(root, rootExtended);
     }
 
     static class PrintTime {
@@ -277,9 +311,7 @@ public class InfinityDBSimpleRestClientTest {
         void printTime(String msg, int len) {
             long t2 = System.currentTimeMillis();
             double time = (t2 - t) / 1e3;
-            System.out.println(msg + " time=" + time
-                    + " len=" + len
-                    + " speed=" + len / time);
+            System.out.format("%30s time=%3.4f len=%6d speed=%5.3f KB/s\r\n", msg, time, len, len / time / 1e3);
             t = t2;
         }
     }
